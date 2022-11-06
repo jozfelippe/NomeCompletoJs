3e660ac7948b6526647e1ee9ba27669b1e9b32e0
Jose Felipe da Silva Santos <josefelipesilvasantos874@gmail.com>
2022-11-06 02:05:08 -0300

    Finalizado

---
+++ index.js
@@ -0,0 +1,5 @@
+let nome = prompt("Digite seu primeiro nome: ");
+let sobrenome = prompt("Digite seu sobrenome: ");
+
+let nomeCompleto = nome + sobrenome;
+alert(`Seu nome completo é: ${nome + " " + sobrenome} \nSeu nome de catálogo é: ${sobrenome.toUpperCase()} ${nome}`);
